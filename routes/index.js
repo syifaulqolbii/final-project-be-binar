2c075c700f2cc80732853b2ae7732d53486445fe
Syifaul Qolbi <syifaul@pop-os.localdomain>
2022-12-16 02:32:43 +0700

    fix router

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.post('/auth/reset-password', con.auth.resetPassword);
 router.get('/list-airport', con.list.listAirport);
 
 // router.get('/home?', con.hom.getData)
-router.get('/search?', mid.cekLogin,con.fli.getSearch)
+router.get('/search?', con.fli.getSearch)
 router.post('/route', mid.cekLogin,rbac(MODUL.AdminDashboard, true, true), con.fli.create)
 router.put('/flight', mid.cekLogin,con.fli.update)
 router.delete('/flight', mid.cekLogin,con.fli.delete)
