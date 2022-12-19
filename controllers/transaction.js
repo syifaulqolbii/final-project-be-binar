a1d8b1e9a2d3b0559f56aec83cc292a1eddeae5d
DonnDoniii <76738183+doniwahyus@users.noreply.github.com>
2022-12-19 20:28:52 +0700

    Update transaction.js

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -157,7 +157,7 @@ module.exports = {
                                 UserId,
                                 TransactionId: transaction.id,
                                 PassengerId: Passenger.id,
-                                passengers: DataPassengers.length
+                                booked_passengers: DataPassengers.length
                             })
                         })
                     }    
