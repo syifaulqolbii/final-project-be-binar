4d3919e211583e62b07bb2a78549968e70ba30a2
Syifaul Qolbi <syifaul@pop-os.localdomain>
2022-12-30 23:43:43 +0700

    feat: fix edit user

--- controllers/auth.js
+++ controllers/auth.js
@@ -309,15 +309,31 @@ module.exports = {
                     id : id
                 }
             });
+
             const updatedUser = await User.findOne({
                 where:{
                     id : id
                 }
             });
+
+            payload = {
+                id: updatedUser.id,
+                name: updatedUser.name,
+                email: updatedUser.email,
+                role: updatedUser.role,
+                gender: updatedUser.gender,
+                phone: updatedUser.phone
+            }
+
+            const token = jwt.sign(payload, JWT_SECRET_KEY)
+
             return res.status(201).json({
                 status: true,
                 message: 'Succes Update Data',
-                data: updatedUser
+                data: {
+                    updatedUser,
+                    token
+                }
             });
         } catch (error) {
             next(error);
