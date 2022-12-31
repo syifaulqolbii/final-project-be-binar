17e809dfa09d6e809c50e014a9d603d5ef5d9bbd
Syifaul Qolbi <syifaul@pop-os.localdomain>
2022-12-31 14:51:47 +0700

    feat: update list airport

---
+++ db/models/list.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class List extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  List.init({
+    name: DataTypes.STRING,
+    region: DataTypes.STRING,
+    iata: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'List',
+  });
+  return List;
+};
